83a8771763cc03b930b0b08df26e3638bc3a6251
Doofenshmirtz-de <Jarno.Kibies@outlook.de>
2025-09-11 19:02:32 +0200

    new desing

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -1,4 +1,5 @@
 import "../globals.css";
+import "../styles/hero-redesign.css";
 import { NextIntlClientProvider } from "next-intl";
 import { notFound } from "next/navigation";
 import { setRequestLocale, getMessages } from "next-intl/server";
@@ -16,6 +17,7 @@ import { AnalyticsProvider } from "../providers/AnalyticsProvider";
 import PWAInstallPrompt from "../ui/PWAInstallPrompt";
 import ServiceWorkerRegistration from "../components/ServiceWorkerRegistration";
 import { CookieBanner } from "../ui/CookieBanner";
+import Script from "next/script";
 
 export function generateStaticParams() {
   return routing.locales.map((locale) => ({ locale }));
@@ -84,6 +86,12 @@ export default async function LocaleLayout(props: LocaleLayoutProps) {
           </SoundProvider>
         </SettingsProvider>
       </AnalyticsProvider>
+      
+      {/* Hero Enhancement Script */}
+      <Script
+        src="/scripts/hero-enhancements.js"
+        strategy="afterInteractive"
+      />
     </NextIntlClientProvider>
   );
 }
